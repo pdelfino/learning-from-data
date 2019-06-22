8b91c7a225899a3ae1f98298a47b635baf41d40c
Pedro Delfino <p.delfino01@gmail.com>
2019-06-22 13:22:31 -0300

    estatísticas básicas

--- ml-project/knn-for-real.py
+++ ml-project/knn-for-real.py
@@ -4,23 +4,31 @@ import pandas as pd
 
 # no arquivo de dados são 11 variáveis, 10 independetes e 1 dependente?
 df = pd.read_csv('breast-cancer-wisconsin.data.txt')
+
 df.replace('?',-99999, inplace=True)
+
 df.drop(['id'], 1, inplace=True)
 
 # as features são tudo menos a coluna de classe
 X = np.array(df.drop(['class'], 1))
+
 y = np.array(df['class'])
 
 # fazer a cross validation, usando os dados de treinamento pra estimar os parâmetros
-X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
+# separar nos conjuntos de treino e de teste, para depois descobrir a acurácia
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.3)
 
 clf = neighbors.KNeighborsClassifier()
+
+# inserir no objeto o treinamento
 clf.fit(X_train, y_train)
+
+# fazer o teste para saber a acurácia
 accuracy = clf.score(X_test, y_test)
-print(accuracy)
+print("acurácia: ",accuracy)
 
 #exemplo pra prever se a célula é benigna ou maligna
 example_measures = np.array([4,2,1,1,1,2,3,2,1])
 example_measures = example_measures.reshape(1, -1)
 prediction = clf.predict(example_measures)
-print(prediction)
+print("predição: ",prediction)
---
+++ ml-project/stats.py
@@ -0,0 +1,16 @@
+import numpy as np
+from sklearn import preprocessing, cross_validation, neighbors
+import pandas as pd
+
+df = pd.read_csv('breast-cancer-wisconsin.data.txt')
+
+for i in df:
+    print (i)
+    print (df[str(i)].describe())
+    print (" ")
+
+""" 
+    excluí o primeiro e último resultado do print pq era o ID e o output final
+    sobre ser maligno ou benigno
+"""
+
