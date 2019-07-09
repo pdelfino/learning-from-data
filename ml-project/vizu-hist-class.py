57bc996f975e2416b0e8fc47b722b039ad135345
Pedro Delfino <p.delfino01@gmail.com>
2019-07-09 18:03:46 -0300

    improve visualization

--- ml-project/vizu-hist-class.py
+++ ml-project/vizu-hist-class.py
@@ -14,5 +14,9 @@ plt.title('Maligno versus Benigno')
 plt.ylabel('Frequência')
 plt.figtext(.75, .01, "4 = Tumor Maligno")
 plt.figtext(.15, .01, "2 = Benigno")
+x = [2,4]
+plt.xticks(x)
+y = [0,50,100,150,200,250,300,350,400,450,500,550,600]
+plt.yticks(y)
 plt.show()
 
